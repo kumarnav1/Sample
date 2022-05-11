88c378c64eb3ce54997a4ef5def455467294d026
nk400 <navkr007@gmail.com>
2022-05-11 09:12:43 +0530

    ADDED: Another Welcome message

--- src/HelloEkta.java
+++ src/HelloEkta.java
@@ -2,5 +2,11 @@ public class HelloEkta {
     public static void main(String[] args) {
 
         System.out.println("Hello Ekta");
+        System.out.println("Welcome to Bridge labz");
+        for (int i = 0; i < 10; i++) {
+            if (true) {
+                System.out.println("hi");
+            }
+        }
     }
 }
