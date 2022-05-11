c42a77436e20c97b6c607feb48820680abce4598
nk400 <navkr007@gmail.com>
2022-05-11 09:24:42 +0530

    ADDED: Another change

--- src/HelloEkta.java
+++ src/HelloEkta.java
@@ -4,9 +4,7 @@ public class HelloEkta {
         System.out.println("Hello Ekta");
         System.out.println("Welcome to Bridge labz");
         for (int i = 0; i < 10; i++) {
-            if (true) {
-                System.out.println("hi");
-            }
+
         }
     }
 }
